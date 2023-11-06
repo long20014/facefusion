f09c94a0ec0d02d4a575cfba26e0d848a12b58cd
long20014 <long20014@yahoo.com>
2023-11-06 21:31:44 +0700

    change some default setting

--- facefusion/uis/components/preview.py
+++ facefusion/uis/components/preview.py
@@ -134,8 +134,8 @@ def update_preview_frame_slider(frame_number : int = 0) -> gradio.Slider:
 
 def process_preview_frame(source_face : Face, reference_face : Face, temp_frame : Frame) -> Frame:
 	temp_frame = resize_frame_dimension(temp_frame, 640, 640)
-	if predict_frame(temp_frame):
-		return cv2.GaussianBlur(temp_frame, (99, 99), 0)
+	# if predict_frame(temp_frame):
+	# 	return cv2.GaussianBlur(temp_frame, (99, 99), 0)
 	for frame_processor in facefusion.globals.frame_processors:
 		frame_processor_module = load_frame_processor_module(frame_processor)
 		if frame_processor_module.pre_process('preview'):
